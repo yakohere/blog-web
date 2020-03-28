554b042abd76d2a094790545d8a526d46c386da3
devyako <yakhyoismoildjonov@gmail.com>
2020-03-28 10:16:57 +0500

    fixing nav

--- src/Components/UI/Navigation.js
+++ src/Components/UI/Navigation.js
@@ -4,12 +4,12 @@ import styled from "styled-components";
 const navigation = props => {
   return (
     <Container>
-      <ul>
-        <li>BLOGS</li>
-        <li>ARTICLES</li>
-        <li>MOTIVATIONS</li>
-        <li>ABOUT</li>
-      </ul>
+      <div className="nav">
+        <a>BLOGS</a>
+        <a>ARTICLES</a>
+        <a>MOTIVATIONS</a>
+        <a>ABOUT</a>
+      </div>
     </Container>
   );
 };
@@ -22,18 +22,16 @@ const Container = styled.div`
   justify-content: center;
   z-index: 400;
   font-family: "Baloo 2", cursive;
-  ul {
+  padding: 10px;
+  box-sizing: border-box;
+  .nav {
     width: 50%;
     display: flex;
     justify-content: space-around;
-    list-style: none;
     @media screen and (max-width: 728px) {
       width: 100%;
     }
-
-    li {
-      font-size: 17px;
-      color: black;
+    a {
     }
   }
 `;
