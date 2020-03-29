f0dd57d5ced0467802b9dfb133d2d39806e03e18
devyako <yakhyoismoildjonov@gmail.com>
2020-03-29 22:49:49 +0500

    fixing bugs

---
+++ src/Components/About.js
@@ -0,0 +1,16 @@
+import React from "react";
+import styled from "styled-components";
+
+const about = () => {
+  return (
+    <Container>
+      <div>Hello</div>
+    </Container>
+  );
+};
+
+export default about;
+
+const Container = styled.div`
+  width: 100%;
+`;
